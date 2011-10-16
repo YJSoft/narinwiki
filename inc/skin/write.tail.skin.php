4c61301c6985ebcc225a0de3e7b1bfdccbeb8682
chongmyung.park <chongmyung.park@bc9a75e1-b6d1-15cf-7f8b-c9978fbd4b50>
2011-10-16 20:43:33 +0000

    2011-10-17 나린위키 도움 라이브러리

--- inc/skin/write.tail.skin.php
+++
@@ -1,8 +0,0 @@
-<?
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
-include_once $wiki[path] . "/inc/inc.write.tail.php";
-
-$board_skin_path = $wiki[skin_path];
-@include_once $wiki[skin_path]."/write.tail.skin.php";
-$board_skin_path = $wiki[inc_skin_path];
-?>
