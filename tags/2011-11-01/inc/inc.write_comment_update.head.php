10311f917b740318bf1a2ea00ce0dd422a28e5da
chongmyung.park <chongmyung.park@bc9a75e1-b6d1-15cf-7f8b-c9978fbd4b50>
2011-10-31 16:16:22 +0000

    2011-11-01 버전

---
+++ tags/2011-11-01/inc/inc.write_comment_update.head.php
@@ -0,0 +1,12 @@
+<?
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가 
+
+extract($wikiEvent->trigger("WRITE_COMMENT_UPDATE_HEAD", array("w"=>$w, 
+																			"wr_id"=>$wr_id, 
+																			"wr_doc"=>stripcslashes($wr_doc),
+																			"wr_name"=>stripcslashes($wr_name), 
+																			"wr_email"=>stripcslashes($wr_email), 
+																			"wr_content"=>stripcslashes($wr_content),
+																			"comment_id"=>$comment_id)));	
+
+?>
---
+++ tags/2011-11-01/inc/skin/write_update.skin.php
@@ -0,0 +1,9 @@
+<?
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가 
+
+include_once $wiki[path]."/inc/inc.write_update.php";
+
+$board_skin_path = $wiki[skin_path];
+@include_once $wiki[skin_path]."/write_update.skin.php";
+$board_skin_path = $wiki[inc_skin_path];
+?>                                                                                            
