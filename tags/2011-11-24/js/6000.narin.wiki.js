45e9ad2e01e357423ceaf9bf760b90c8db2436b5
chongmyung.park <chongmyung.park@bc9a75e1-b6d1-15cf-7f8b-c9978fbd4b50>
2011-11-23 20:34:51 +0000

    2011-11-24 버전 태깅

---
+++ tags/2011-11-24/js/6000.narin.wiki.js
@@ -0,0 +1,323 @@
+/**
+ * 나린위키 공용 자바 스크립트
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     byfun (http://byfun.com)
+ */
+ 
+$(document).ready(function() {
+	$(".wiki_content img").removeAttr("onclick");
+	$(".wiki_content img").each(function() {
+		if($(this).parent().get(0).tagName != "A")
+			$(this).wrap("<a href='"+$(this).attr('src')+"' class='wiki_modal'></a>");
+	});
+
+	$.nmAnims({
+		fade: {
+			showBg: function(nm, clb) {
+				nm.elts.bg.fadeTo(250, 0.35, clb);
+			}
+    }
+  });		
+
+	$(".wiki_modal").nm();
+  
+  $("#toc_fold").click(function(evt) {
+  	evt.preventDefault();
+  	$("#wiki_toc_content").slideToggle();
+  	head = $("#wiki_toc_head a");
+  	if(head.hasClass("fold_up")) {
+  		head.removeClass("fold_up").addClass("fold_down");
+  	} else head.removeClass("fold_down").addClass("fold_up");
+  });  
+  
+  $(".button a, .button input").click(function() {
+  	$(this).blur();
+  }); 
+
+	// 글쓰기의 에디터 툴바
+	if(!is_comment) {
+		$(".wr_content").narinEditor('wiki_write');
+		$(".markItUp .narin_media").click(function() {
+			win_pop_center(wiki_path + '/media.php?bo_table='+g4_bo_table, 'media manager', 1000, 600, 'toolbar=0,resizable=1,scrollbars=1');
+		});
+	}
+		    
+});
+
+
+/**
+ * @param docname 경로는 제외한 문서명
+ */
+function check_doc_name(docname, hidemsg)
+{
+	if($.trim(docname) == '') {
+		if(!hidemsg) alert("문서명을 입력하세요");
+		return false;
+	}	
+	var pattern = /[\|\/\\\\]/;
+	if(pattern.test(docname)) {
+		if(!hidemsg) alert("문서명에 다음 문자는 사용할 수 없습니다 : \\, |, /");
+		return false;
+	}		
+	return true;
+}
+
+/**
+ * @param foldername 폴더 경로 전체
+ */
+function check_folder_name(foldername, hidemsg)
+{
+	if($.trim(foldername) == '') {
+		if(!hidemsg) alert("폴더명을 입력하세요");
+		return false;
+	}
+	var pattern = /[\|\\\\]/;
+	if(pattern.test(foldername)) {
+		if(!hidemsg) alert("폴더명에 다음 문자는 사용할 수 없습니다 : \\, |");
+		return false;
+	}
+	
+	pattern = /[\/]{2,}/;
+	if(pattern.test(foldername)) {
+		if(!hidemsg) alert("폴더명에 / 를 연속하여 사용할 수 없습니다.");
+		return false;
+	}	
+	
+	pattern = /^\//;
+	if(!pattern.test(foldername)) {
+		if(!hidemsg) alert("폴더명은 / 로 시작하는 문자열 이어야 합니다.");
+		return false;
+	}	
+	
+	return true;
+}
+
+function recover_history(wr_id, hid)
+{
+	if(confirm("이 문서로 복원하시겠습니까?")) {
+		$.post(wiki_path+"/exe/history.php", { bo_table : g4_bo_table, w : 'r', wr_id : wr_id, hid : hid}, function(data) {
+			if(data == 1) {
+				location.href = g4_url + "/" + g4_bbs + "/board.php?bo_table=" + g4_bo_table + "&wr_id=" + wr_id;
+			} else {
+				alert("문서 이력 복원를 못하였습니다.");
+			}
+		});
+	}
+}
+
+function delete_history(hid)
+{
+	if(confirm("이 문서 이력을 삭제하시겠습니까?")) {
+		$.post(wiki_path+"/exe/history.php", { bo_table : g4_bo_table, w : 'd', hid : hid}, function(data) {
+			if(data == 1) {
+				location.reload();
+			} else {
+				alert("문서 이력 삭제를 못하였습니다.");
+			}
+		});
+	}
+}
+
+function clear_history(wr_id)
+{
+	if(confirm("이 문서의 모든 이력을 삭제하시겠습니까?\n페이지에 표시되지 않는 이력도 삭제됩니다.")) {
+		$.post(wiki_path+"/exe/history.php", { w : 'da', bo_table : g4_bo_table, wr_id : wr_id}, function(data) {
+			if(data == 1) {
+				location.href = g4_url + "/" + g4_bbs + "/board.php?bo_table=" + g4_bo_table + "&wr_id=" + wr_id;
+			} else {
+				alert("문서 이력 삭제를 못하였습니다.");
+			}
+		});
+	}	
+}
+
+function delete_selected_history(wr_id)
+{
+	var hids = [];
+	var chks = $("input[@name='hid[]']:checked").map(function() {
+		hids.push(this.value);
+	});
+	
+  if (hids.length == 0) 
+  {
+      alert("문서를 하나 이상 선택하세요.");
+      return false;
+  }	
+	
+	if(confirm("선택한 문서 이력을 삭제하시겠습니까?")) {		
+		$.post(wiki_path+"/exe/history.php", { w : 'ds', bo_table : g4_bo_table, wr_id : wr_id, hids : hids}, function(data) {
+			if(data == 1) {
+				location.reload();
+			} else {
+				alert("문서 이력 삭제를 못하였습니다.");				
+			}
+		});
+	}	
+}
+
+function delete_selected_changes()
+{
+	var cids = [];
+	var chks = $("input[@name='cid[]']:checked").map(function() {
+		cids.push(this.value);
+	});
+	
+  if (cids.length == 0) 
+  {
+      alert("변경내역을 하나 이상 선택하세요.");
+      return false;
+  }	
+	
+	if(confirm("선택한 변경내역을 삭제하시겠습니까?")) {		
+		$.post(wiki_path+"/exe/changes.php", { w : 'ds', bo_table : g4_bo_table, cids : cids}, function(data) {
+			if(data == 1) {
+				location.reload();
+			} else {
+				alert("변경내역 삭제를 못하였습니다.");				
+			}
+		});
+	}	
+}
+
+function clear_changes()
+{
+	if(confirm("모든 변경내역을 삭제하시겠습니까?\n페이지에 표시되지 않는 내역도 모두 삭제됩니다.")) {
+		$.post(wiki_path+"/exe/changes.php", { w : 'da', bo_table : g4_bo_table}, function(data) {
+			if(data == 1) {
+				location.reload();
+			} else {
+				alert("변경내역 삭제를 못하였습니다.");
+			}
+		});
+	}	
+}
+
+function createDoc(folder)
+{
+	if(!check_folder_name(folder)) return;
+	var doc = prompt('문서명 입력 : ', '');
+	if(doc != null) {
+		if(!check_doc_name(doc)) {
+			createDoc(g4_bo_table, folder, wiki_path);
+			return;
+		}
+		docpath = ( folder == "/" ? "/" : folder+"/") + doc;
+		location.href = wiki_path + "/narin.php?bo_table="+g4_bo_table+"&doc=" + encodeURIComponent(docpath);
+	}
+}
+
+function wiki_search(f)
+{
+	if($.trim(f.stx.value) == '') {
+		alert("검색어를 입력하세요");
+		return false;
+	}
+	return true;
+}
+
+function wiki_dialog(title, msg, options)
+{
+	settings = { 
+		msg_id : 'wiki_dialog', 
+		title_bgcolor : "#555", 
+		title_color : "#fff", 
+		closeOnClick : false,
+		closeOnEscape : false,		
+		buttons : '<span class="button"><a href="javascript:$.nmTop().close();">확인</a></span>',
+		onClose : function() {} 
+	};	
+	jQuery.extend(settings, options);	
+	
+	msgLayer = $("<div></div>")
+						.attr('style', 'display:none;')
+						.attr('id', settings.msg_id)
+						.html([
+							'<div style="padding:5px 10px;background-color:'+settings.title_bgcolor+';color:'+settings.title_color+';font-weight:bold;">',
+							title,
+							'</div>',
+							'<div style="padding:10px;line-height:160%;">',
+							msg,
+							'</div>',
+							'<div style="margin-top:10px;border-top:1px dashed #ccc;padding-top:10px;text-align:center">',
+							settings.buttons,
+							'</div>',
+							'<a href="#'+settings.msg_id+'" id="btn_'+settings.msg_id+'" style="display:none"></a>'
+							].join(''));
+					$(document.body).prepend(msgLayer);
+					$("#btn_"+settings.msg_id).nm({closeOnClick : settings.closeOnClick, closeOnEscape : settings.closeOnClick, closeButton : '', callbacks : {
+							afterClose : function() {
+								msgLayer.remove();
+								settings.onClose();
+							}
+						} }).nmCall();
+	
+}
+
+function wiki_msg(msg, options) {
+	
+	settings = { msg_id : 'wiki_msg', seconds : 2500, bgcolor : "#555", color : "#fff", callback : function() {} };
+	jQuery.extend(settings, options);
+	msgLayer = $("<div></div>")
+		.attr('style', 'display:none;position:absolute;padding:10px 30px;text-align:center;background-color:'+settings.bgcolor+';color:'+settings.color+';z-index:999999')
+		.html(msg);
+	$(document.body).prepend(msgLayer);
+	msgLayer.center();
+	msgLayer.fadeIn();
+	setTimeout(function() { msgLayer.fadeOut(function() { msgLayer.remove(); settings.callback(); });  }, settings.seconds);			
+}
+
+function objToString(o){
+	var parse = function(_o){    
+		var a = [], t;        
+		for(var p in _o){        
+			if(_o.hasOwnProperty(p)){            
+				t = _o[p];                
+				if(t && typeof t == "object"){                
+					a[a.length]= p + ":{ " + arguments.callee(t).join(", ") + "}";                    
+				}
+				else {                    
+					if(typeof t == "string"){                    
+					  a[a.length] = [ p+ ": \"" + t.toString() + "\"" ];
+					}
+					else{
+					  a[a.length] = [ p+ ": " + t.toString()];
+					}                    
+				}
+			}
+		}        
+		return a;        
+	}    
+	return "{" + parse(o).join(", ") + "}";    
+}
+
+(function($){
+	$.center = function($this) {
+		var win = $(window);
+		var top = (win.height() - $this.outerHeight()) / 2;
+		var left = (win.width() - $this.outerWidth()) / 2;
+		var pos = 'fixed';
+		if($.browser.msie) {
+			top += win.scrollTop() || 0;
+			top = (top > 0 ? top : 0);
+			left += win.scrollLeft() || 0;
+			left = (left > 0 ? left : 0);			
+			pos = 'absolute';		
+		}
+		$this.css({position:pos, top : top, left : left});				  			
+	};
+	
+	$.fn.center = function() {
+		return this.each(function() {
+			var ss = function() { $.center($(this)); };
+			$(window).resize(ss).scroll(ss);			 	    
+			ss();
+		});		
+	};
+	
+	$.fn.center_now = function() {
+		return this.each(function() {
+			$.center($(this));
+		});
+	};
+})(jQuery);
---
+++ tags/2011-11-24/skin/board/basic/write.skin.php
@@ -0,0 +1,391 @@
+<?
+/**
+ * 나린위키 스킨 : 문서 작성 스킨
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     byfun (http://byfun.com)
+ */
+ 
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+?>
+
+<script type="text/javascript">
+// 글자수 제한
+var char_min = parseInt(<?=$write_min?>); // 최소
+var char_max = parseInt(<?=$write_max?>); // 최대
+</script>
+
+<form name="fwrite" method="post" onsubmit="return fwrite_submit(this);" enctype="multipart/form-data" style="margin:0px;">
+<input type=hidden name=null> 
+<input type=hidden name=w        value="<?=$w?>">
+<input type=hidden name=bo_table value="<?=$bo_table?>">
+<input type=hidden name=wr_id    value="<?=$wr_id?>">
+<input type=hidden name=sca      value="<?=$sca?>">
+<input type=hidden name=sfl      value="<?=$sfl?>">
+<input type=hidden name=stx      value="<?=$stx?>">
+<input type=hidden name=spt      value="<?=$spt?>">
+<input type=hidden name=sst      value="<?=$sst?>">
+<input type=hidden name=sod      value="<?=$sod?>">
+<input type=hidden name=page     value="<?=$page?>">
+<input type=hidden name=wr_doc     value="<?=$wr_doc?>">
+<input type=hidden name=wr_subject id="wr_subject" itemname="제목" value="<?=$subject?>">
+
+	<div style="font-weight:bold; font-size:14pt; padding:8px"><?=$title_msg?> : <?=$wr_doc?></div>
+	
+	<div style="margin:8px 0;padding:5px;border-bottom:1px dashed #ccc;font-size:9pt;">
+		문서를 작성하시고 완료 버튼을 클릭하세요.
+		위키 문법에 대해서 더 알려면 <a href="http://narin.byfun.com/syntax">위키 문법 학습</a> 페이지를 참고하세요.
+	</div>
+	
+	<table width="100%" class="write_table" border="0" cellspacing="0" cellpadding="0">
+	<? if ($is_name) { ?>
+	<tr>
+	    <th>이 름</th>
+	    <td><input class='ed' maxlength=20 size=15 name=wr_name itemname="이름" required value="<?=$name?>"></td>
+	</tr>
+	<? } ?>
+	
+	<? if ($is_password) { ?>
+	<tr>
+	    <th>패스워드</th>
+	    <td><input class='ed' type=password maxlength=20 size=15 name=wr_password itemname="패스워드" <?=$password_required?>></td>
+	</tr>
+	<? } ?>
+	
+	<? if ($is_email) { ?>
+	<tr>
+	    <th>이메일</th>
+	    <td><input class='ed' maxlength=100 size=50 name=wr_email email itemname="이메일" value="<?=$email?>"></td>
+	</tr>
+	<? } ?>
+	
+	<? if ($is_homepage) { ?>
+	<tr>
+	    <th>홈페이지</th>
+	    <td><input class='ed' size=50 name=wr_homepage itemname="홈페이지" value="<?=$homepage?>"></td>
+	</tr>
+	<? } ?>
+	
+	<? 
+	$option = "";
+	$option_hidden = "";
+	if ($is_notice || $is_html || $is_secret || $is_mail) { 
+	    $option = "";
+	    if ($is_notice) { 
+	        //$option .= "<input type=checkbox name=notice value='1' $notice_checked>공지&nbsp;";
+	    }
+	
+	    if ($is_html) {
+	        if ($is_dhtml_editor) {
+	            $option_hidden .= "<input type=hidden value='html1' name='html'>";
+	        } else {
+	            $option_hidden .= "<input type=hidden value='html1' name='html' $html_checked>";
+	        }
+	    }
+	
+	    if ($is_secret) {
+	        if ($is_admin || $is_secret==1) {
+	            $option .= "<input type=checkbox value='secret' name='secret' $secret_checked><span class=w_title>비밀글</span>&nbsp;";
+	        } else {
+	            $option_hidden .= "<input type=hidden value='secret' name='secret'>";
+	        }
+	    }
+	    
+	    if ($is_mail) {
+	        $option .= "<input type=checkbox value='mail' name='mail' $recv_email_checked>답변메일받기&nbsp;";
+	    }
+	}
+	echo $option_hidden;
+	if ($option) {
+	?>
+	<tr>
+	    <th>옵 션</th>
+	    <td><?=$option?></td>
+	</tr>
+	<? } ?>
+	
+	<tr>
+	    <th>문서요약</th>
+	    <td><input class='ed' name="wr_history" size="80" name=wr_name itemname="문서요약" ></td>
+	</tr>
+	</table>
+	
+	<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+	    <td>
+	        <textarea id="wr_content" name="wr_content" class="wr_content tx" style='width:100%;height:350px; word-break:break-all;' itemname="내용" required 
+	        <? if ($write_min || $write_max) { ?>onkeyup="check_byte('wr_content', 'char_count');"<?}?>><?=$content?></textarea>
+	        <? if ($write_min || $write_max) { ?><script type="text/javascript"> check_byte('wr_content', 'char_count'); </script><?}?>
+	    </td>
+	</tr>
+	</table>
+	
+	<table width="100%" class="write_table" border="0" cellspacing="0" cellpadding="0">        
+	<? 
+	// 미디어 관리자 기능추가로 파일을 사용하지 않습니다.
+	// 사용하시려면 다음 if 문의 '0 &&' 를 지워주세요.
+	if (0 && $is_file) { 
+	?>
+	<tr>
+		<th style="padding-top:10px; line-height:20px;">
+		    파일첨부 <br/>
+		    <span onclick="add_file();" style="cursor:pointer;"><img src="<?=$board_skin_path?>/img/btn_file_add.gif"></span> 
+		    <span onclick="del_file();" style="cursor:pointer;"><img src="<?=$board_skin_path?>/img/btn_file_minus.gif"></span>
+		</th>
+		<td style='padding:5 0 5 0;'>
+		<table id="variableFiles" cellpadding=0 cellspacing=0></table>
+	        <script type="text/javascript">
+	        var flen = 0;
+	        function add_file(delete_code)
+	        {
+	            var upload_count = <?=(int)$board[bo_upload_count]?>;
+	            if (upload_count && flen >= upload_count)
+	            {
+	                alert("이 게시판은 "+upload_count+"개 까지만 파일 업로드가 가능합니다.");
+	                return;
+	            }
+	
+	            var objTbl;
+	            var objRow;
+	            var objCell;
+	            if (document.getElementById)
+	                objTbl = document.getElementById("variableFiles");
+	            else
+	                objTbl = document.all["variableFiles"];
+	
+	            objRow = objTbl.insertRow(objTbl.rows.length);
+	            objCell = objRow.insertCell(0);
+	
+	            objCell.innerHTML = "<input type='file' class='ed' name='bf_file[]' title='파일 용량 <?=$upload_max_filesize?> 이하만 업로드 가능'>";
+	            if (delete_code)
+	                objCell.innerHTML += delete_code;
+	            else
+	            {
+	                <? if ($is_file_content) { ?>
+	                objCell.innerHTML += "<br><input type='text' class='ed' size=50 name='bf_content[]' title='업로드 이미지 파일에 해당 되는 내용을 입력하세요.'>";
+	                <? } ?>
+	                ;
+	            }
+	
+	            flen++;
+	        }
+	
+	        <?=$file_script; //수정시에 필요한 스크립트?>
+	
+	        function del_file()
+	        {
+	            // file_length 이하로는 필드가 삭제되지 않아야 합니다.
+	            var file_length = <?=(int)$file_length?>;
+	            var objTbl = document.getElementById("variableFiles");
+	            if (objTbl.rows.length - 1 > file_length)
+	            {
+	                objTbl.deleteRow(objTbl.rows.length - 1);
+	                flen--;
+	            }
+	        }
+	        </script>
+		</td>
+	</tr>
+	
+	<? } ?>
+	
+	<? if ($is_trackback) { ?>
+	<tr>
+	  <th>트랙백주소</th>
+	  <td><input class='ed' size=50 name=wr_trackback itemname="트랙백" value="<?=$trackback?>">
+	      <? if ($w=="u") { ?><input type=checkbox name="re_trackback" value="1">핑 보냄<? } ?>
+		</td>
+	</tr>
+	<? } ?>
+	
+	<? if ($is_guest) { ?>
+	<tr>
+	  <th>보안이미지</th>
+	  <td>
+	  <input class='ed' type=input size=10 name=wr_key itemname="자동등록방지" required><img id='kcaptcha_image' /><span style="margin-left:85px">왼쪽의 글자를 입력하세요.</span>
+	  </td>
+	</tr>
+	<? } ?>
+	
+	<tr>
+		<td colspan="2" style="text-align:center">
+			<? if($member[mb_id]) { ?>
+			<span class="button green"><a href="#tmpsave" id="btn_tmpsave">임시저장</a></span>&nbsp;
+			<? } ?>
+			<span class="button red"><input type=submit id="btn_submit" value="완료" border=0 accesskey='s'></span>&nbsp;
+	    <span class="button"><a href="javascript:history.go(-1);" id="btn_back">뒤로</a></span>&nbsp;
+	    <span class="button"><a href="<?=$wiki[path]?>/narin.php?bo_table=<?=$wiki[bo_table]?>" id="btn_list">시작페이지</a></span></td>
+	  </td>
+	 </tr>
+	</table>
+
+</form>
+
+<script type="text/javascript" src="<?="$g4[path]/js/jquery.kcaptcha.js"?>"></script>
+<script type="text/javascript">
+<?
+// 관리자라면 분류 선택에 '공지' 옵션을 추가함
+if ($is_admin) 
+{
+    echo "
+    if (typeof(document.fwrite.ca_name) != 'undefined')
+    {
+        document.fwrite.ca_name.options.length += 1;
+        document.fwrite.ca_name.options[document.fwrite.ca_name.options.length-1].value = '공지';
+        document.fwrite.ca_name.options[document.fwrite.ca_name.options.length-1].text = '공지';
+    }";
+} 
+?>
+
+with (document.fwrite) 
+{
+    if (typeof(wr_name) != "undefined")
+        wr_name.focus();
+    else if (typeof(wr_subject) != "undefined")
+        wr_content.focus();
+    else if (typeof(wr_content) != "undefined")
+        wr_content.focus();
+
+    if (typeof(ca_name) != "undefined")
+        if (w.value == "u")
+            ca_name.value = "<?=$write[ca_name]?>";
+}
+
+function html_auto_br(obj) 
+{
+    if (obj.checked) {
+        result = confirm("자동 줄바꿈을 하시겠습니까?\n\n자동 줄바꿈은 게시물 내용중 줄바뀐 곳을<br>태그로 변환하는 기능입니다.");
+        if (result)
+            obj.value = "html2";
+        else
+            obj.value = "html1";
+    }
+    else
+        obj.value = "";
+}
+
+function fwrite_submit(f) 
+{
+    if (document.getElementById('char_count')) {
+        if (char_min > 0 || char_max > 0) {
+            var cnt = parseInt(document.getElementById('char_count').innerHTML);
+            if (char_min > 0 && char_min > cnt) {
+                alert("내용은 "+char_min+"글자 이상 쓰셔야 합니다.");
+                return false;
+            } 
+            else if (char_max > 0 && char_max < cnt) {
+                alert("내용은 "+char_max+"글자 이하로 쓰셔야 합니다.");
+                return false;
+            }
+        }
+    }
+
+    if (document.getElementById('tx_wr_content')) {
+        if (!ed_wr_content.outputBodyText()) { 
+            alert('내용을 입력하십시오.'); 
+            ed_wr_content.returnFalse();
+            return false;
+        }
+    }
+
+    var subject = "";
+    var content = "";
+    $.ajax({
+        url: "<?=$wiki[path]?>/exe/ajax.filter.php",
+        type: "POST",
+        data: {
+            "subject": f.wr_subject.value,
+            "content": f.wr_content.value
+        },
+        dataType: "json",
+        async: false,
+        cache: false,
+        success: function(data, textStatus) {
+            subject = data.subject;
+            content = data.content;
+        }
+    });
+
+    if (subject) {
+        alert("제목에 금지단어('"+subject+"')가 포함되어있습니다");
+        f.wr_subject.focus();
+        return false;
+    }
+
+    if (content) { 
+        alert("내용에 금지단어('"+content+"')가 포함되어있습니다");
+        if (typeof(ed_wr_content) != "undefined") 
+            ed_wr_content.returnFalse();
+        else 
+            f.wr_content.focus();
+        return false;
+    }
+
+    if (!check_kcaptcha(f.wr_key)) {
+        return false;
+    }
+
+    document.getElementById('btn_submit').disabled = true;
+    document.getElementById('btn_list').disabled = true;
+
+    <?
+    if ($g4[https_url])
+        echo "f.action = '$g4[https_url]/$g4[bbs]/write_update.php';";
+    else
+        echo "f.action = '$g4[bbs_path]/write_update.php';";
+    ?>   
+		
+		$(window).unbind('beforeunload');    
+ 
+    return true;
+}
+</script>
+<? if($member[mb_id]) { ?>
+<script type="text/javascript">
+	var tmp_saved;
+	
+	function wiki_read_tmpsave() {
+		$("#wr_content").val(tmp_saved);
+		$.nmTop().close();
+	}
+	
+	function wiki_close_tmpsave() {		
+		$.nmTop().close();
+	}
+	function wiki_delete_tmpsave() {
+		$.post(wiki_path+"/exe/a.php", { bo_table : g4_bo_table, w : 'tmpsave_delete', wr_doc : wiki_doc}, function(data) {});
+		$.nmTop().close();
+	}		
+	
+	$(document).ready(function() {
+		$.getJSON(wiki_path+"/exe/a.php", { bo_table : g4_bo_table, w : 'tmpsave_read', wr_doc : wiki_doc}, function(json) {			
+			if(json.code == 1) {					
+				tmp_saved = json.wr_content;
+				wiki_dialog('확인', '임시 저장된 문서가 있습니다.<br/>('+json.wr_date+')', {
+					buttons : [
+						'<span class="button green"><a href="javascript:wiki_read_tmpsave();">읽어오기</a></span>&nbsp;',
+						'<span class="button purple"><a href="javascript:wiki_delete_tmpsave();">삭제</a></span>&nbsp;',						
+						'<span class="button"><a href="javascript:wiki_close_tmpsave();">닫기</a></span>&nbsp;'					
+					].join('')
+				});
+			}
+		});
+	});
+
+	$("#btn_tmpsave").click(function(evt) {
+		evt.preventDefault();
+		$("#btn_tmpsave").hide();
+		$.post(wiki_path+"/exe/a.php", { bo_table : g4_bo_table, w : 'tmpsave_write', wr_doc : wiki_doc, wr_content : $("#wr_content").val()}, function(data) {
+			msg = "저장하지 못했습니다.";
+			bgcolor = "#800000";
+			if(data == 1) {
+				msg = "저장하였습니다.";
+				bgcolor = "#333";
+			}
+			wiki_msg(msg, {bgcolor:bgcolor, seconds : 2000, callback : function() { $("#btn_tmpsave").show(); } });
+		});
+	});
+</script>
+<?}?>
+<script type="text/javascript" src="<?="$g4[path]/js/board.js"?>"></script>
+<script type="text/javascript"> window.onload=function() { drawFont(); } </script>
